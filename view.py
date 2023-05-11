b81b7b6e6dcdfd123d608ebb91f898614da30c83
Guilherme433 <guilherme.cn.pc@gmail.com>
2023-05-11 12:43:11 +0100

    Update view.py

--- view.py
+++ view.py
@@ -8,10 +8,12 @@ class View:
         self.frame.pack(fill=tk.BOTH, expand=True)
 
 
-        self.user_label = tk.Label(self.frame, text=("Nomem de Utilizador"), bg=("black"), foreground=("white"), font=("Arial", 15), pady=5)
+        self.user_label = tk.Label(self.frame, text=("Nome de Utilizador"), bg=("black"), foreground=("white"), font=("Arial", 15), pady=5)
         self.user_label.pack()
         self.user_entry = tk.Entry(self.frame, width=25)
         self.user_entry.pack()
         self.password = tk.Label(self.frame, text=("Password"), bg=("black"), foreground=("white"), font=("Arial", 15), pady=5)
         self.password.pack()
+        self.password_entry = tk.Entry(self.frame, width=25)
+        self.password_entry.pack()
 
