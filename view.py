e5f77086608f26f72c9815fd8edd11f03d613f8b
sebasgim975 <127939480+sebasgim975@users.noreply.github.com>
2023-05-11 13:43:58 +0100

    Update view.py

--- view.py
+++ view.py
@@ -9,6 +9,7 @@ class View:
         self.master = master
         self.frame = tk.Frame(self.master, bg="gray", padx=75, pady=75)
         self.frame.pack(fill=tk.BOTH, expand=True)
+        self.users = ClientLinkedList()
 
 
         self.user_label = tk.Label(self.frame, text=("Nome de Utilizador"), bg=("gray"), foreground=("black"), font=("Arial", 15), pady=5)
@@ -26,3 +27,12 @@ class View:
 
         self.login_button = tk.Button(self.frame,text="Login",bg="black",foreground="white",font=("Arial",12),width=10)
         self.login_button.pack(pady=5)
+
+    def registar(self):
+        nome = self.user_entry.get()
+        if self.users.find_username(nome) != -1:
+            messagebox.showerror("Erro", "Username existe")
+        elif self.user_entry.size==0:
+            posicao = self.users.size
+            
+
